67d19f61a5cfcce52a36661b9d79df85c1be5427
Weike Liu <weikeliu@Weikes-Air.gateway>
2016-06-29 21:12:08 +1000

    set TAB to be global company-complete

--- init-company.el
+++ init-company.el
@@ -1,3 +1,7 @@
 ;;Configuration of company mode starts here
 (require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
+(defun my:company-config ()
+  (global-company-mode t)
+  (global-set-key (kbd "TAB") 'company-complete))
+
+(add-hook 'after-init-hook 'my:company-config)
